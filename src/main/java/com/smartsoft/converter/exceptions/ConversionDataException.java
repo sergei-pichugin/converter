a00e649375fa526c8c4f58adc3cc16cb8dcfcf30
Sergei Pichugin <spichugin@inbox.ru>
2021-10-16 14:09:55 +0300

    simple demo version

--- src/main/java/com/smartsoft/converter/exceptions/ConversionDataException.java
+++
@@ -1,7 +0,0 @@
-package com.smartsoft.converter.exceptions;
-
-public class ConversionDataException extends Exception {
-    public ConversionDataException(String message) {
-        super(message);
-    }
-}
